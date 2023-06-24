66c1a612572c9dba4950e58e2c6fa31736e934b0
Douglas Barreto <dosideveloper@gmail.com>
2023-06-23 22:25:31 -0400

    update utils supabase

--- utils/utils_supabase.js
+++ utils/utils_supabase.js
@@ -1,42 +1,25 @@
 import supabase from "./init_supabase";
 
+/**
+ *
+ * This function will get every data from a table on a JSON string
+ * @param {string} target
+ * @param {string} select
+ * @returns query
+ */
 export async function getSupabase(target, select) {
-    return await supabase
-        .from(target)
-        .select(select)
-
-    // This function will get every data from a table on a JSON string
+  return await supabase.from(target).select(select);
 }
 
-export async function postSupabase(target, json) {
-    return await supabase
-        .from(target)
-        .insert(json)
+/**
+ *
+ * This function download the post get by its URL
+ * @param {string} target
+ * @param {String} post_url
+ * @returns Blob with post
+ */
+export async function getPostSupabase(target, post_url) {
+  return await supabase.storage.from("blog_storage").download(post_url);
 
-    // The json could be something like this { id: 1, name: 'Denmark' }
+  //
 }
-
-export async function updateSupabase(target, json, filter) {
-    return await supabase
-        .from(target)
-        .update(json)
-        .eq(filter)
-
-    //filter always be in this format: field, data (ex. id, "1")
-}
-
-export async function upsertSupabase(target, json, select) {
-    return await supabase
-        .from(target)
-        .upsert(json)
-        .select(select)
-}
-
-export async function deleteSupabase(target, filter) {
-    return await supabase
-        .from(target)
-        .delete()
-        .eq(filter)
-}
-
-// docs: https://supabase.com/docs/reference/javascript/select
