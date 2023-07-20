da6d21574d2869038f748824b7a7bcf52fda5395
Douglas Barreto <dosideveloper@gmail.com>
2023-07-19 21:48:03 -0400

    Fix: Unfound import on post

--- components/post.jsx
+++ components/post.jsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
 
 export function Post_miniature({ title }) {
   return (
